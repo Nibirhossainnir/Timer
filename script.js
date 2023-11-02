4088235ad6713a6f23e56a6b1e83f774337e47be
Nibirhossainnir <144318970+Nibirhossainnir@users.noreply.github.com>
2023-11-02 20:03:20 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+var cClose = document.querySelector("#c-close");
+var conteiner = document.querySelector(".conteiner");
+var stt = document.querySelector("#stt");
+var conteiner2 = document.querySelector(".conteiner2");
+var min = document.querySelector("#min");
+var sec = document.querySelector("#sec");
+var alarmDiv = document.querySelector(".alarm");
+var stopa = document.querySelector("#stop");
+var audio = new Audio("alarm-clock-23772.mp3")
+cMin = 0;
+cSec = 0;
+timer = null;
+cClose.addEventListener("click", function(){
+    conteiner.classList.remove("c-show");
+    conteiner2.style.filter = "blur(0)";
+})
+stt.addEventListener("click", function(){
+    conteiner.classList.add("c-show");
+    conteiner2.style.filter = "blur(2px)";
+})
+set.addEventListener("click", function(){
+    if(sec.value <60){
+    s.innerHTML = parseInt(sec.value);
+    cSec = parseInt(sec.value);
+    m.innerHTML = parseInt(min.value);
+    cMin = parseInt(min.value);
+    conteiner.classList.remove("c-show");
+    conteiner2.style.filter = "blur(0)";
+    }
+    else{
+        alert("Cannot be greater than 59 seconds.");
+    }
+    if(min.value == ""){
+        m.innerHTML = 0;
+        cMin = 0;
+    }
+    else if(sec.value == ""){
+        cSec = 0;
+        s.innerHTML = 0;
+    }
+})
+reset.addEventListener("click", function(){
+    s.innerHTML = 0;
+    cSec = 0;
+    m.innerHTML = 0;
+    cMin = 0;
+    clearInterval(timer);
+})
+start.addEventListener("click", function(){
+    if(cSec == 0 && cMin>0){
+        cMin--;
+        cSec = 60;
+       }
+       if( timer !== null){
+        clearInterval(timer);
+    }
+    timer = setInterval(() => {
+        if(cSec>0 || cMin > 0){
+            cSec--;
+           s.innerHTML = cSec;
+           m.innerHTML = cMin;
+           if(cMin == 0 && cSec==0){
+            audio.play();
+            audio.loop = true;
+            clearInterval(timer);
+            alarmDiv.classList.add("alarm-show");
+            conteiner2.style.filter = "blur(2px)";
+           }
+           if(cSec == 0 && cMin>0){
+            cMin--;
+            cSec = 60;
+           }
+        }
+        
+    },1000);
+    
+})
+stopa.addEventListener("click", function(){
+    s.innerHTML = 0;
+    cSec = 0;
+    m.innerHTML = 0;
+    cMin = 0;
+    audio.pause();
+    audio.currentTime = 0;
+    alarmDiv.classList.remove   ("alarm-show");
+            conteiner2.style.filter = "blur(0)";
+})
+
+
+
